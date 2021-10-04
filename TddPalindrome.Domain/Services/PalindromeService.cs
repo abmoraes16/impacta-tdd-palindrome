f0b3bfe77fc08c9a693dbdc8296c5fbd34516a84
abmoraes16 <a.bmoraes@yahoo.com>
2021-10-03 22:01:52 -0300

    Add service

---
+++ TddPalindrome.Domain/Services/PalindromeService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TddPalindrome.Domain.Services
+{
+    public class PalindromeService
+    {
+        public bool IsValid(string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
